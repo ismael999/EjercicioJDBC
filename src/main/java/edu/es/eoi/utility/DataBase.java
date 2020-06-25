8c760ae235e29d9e5ad69a9dc5f391a9aafc6a68
Ismael Dominguez Moreno <ismaeldominguezmoreno@gmail.com>
2020-06-25 19:10:19 +0200

    Comentario sobre la URL de la base de datos

--- src/main/java/edu/es/eoi/utility/DataBase.java
+++ src/main/java/edu/es/eoi/utility/DataBase.java
@@ -1,6 +1,7 @@
 package edu.es.eoi.utility;
 
 public enum DataBase {
+	// La ultima parte de la URL es para quitar los avisos al conectar con la Base de Datos.
 	URL("jdbc:mysql://localhost:3306/eoi?serverTimezone=UTC" + "&useSSL=false"),
 	USER("root"),
 	PASS("1234");
